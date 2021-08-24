929f73ac1ca6ff99b71efb5fb93f05428725ee73
tieusuphu <vodanhpro170@gmail.com>
2021-08-24 12:39:56 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,13 +3,13 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from tracker import get_prices
 
-telegram_bot_token = "your-telegram-token"
+telegram_bot_token = "1961253995:AAHxV1L8VKhRMB-ED99u2BccA8BohBaqEH0"
 
 updater = Updater(token=telegram_bot_token, use_context=True)
 dispatcher = updater.dispatcher
 
 
-def start(update, context):
+def Price(update, context):
     chat_id = update.effective_chat.id
     message = ""
 
@@ -19,10 +19,10 @@ def start(update, context):
         price = crypto_data[i]["price"]
         change_day = crypto_data[i]["change_day"]
         change_hour = crypto_data[i]["change_hour"]
-        message += f"Coin: {coin}\nPrice: ${price:,.2f}\nHour Change: {change_hour:.3f}%\nDay Change: {change_day:.3f}%\n\n"
+        message += f"Coin: {coin}\nGiá: ${price:,.2f}\nThay đổi trong giờ: {change_hour:.3f}%\nThay Đổi Trong Ngày: {change_day:.3f}%\n\n"
 
     context.bot.send_message(chat_id=chat_id, text=message)
 
 
-dispatcher.add_handler(CommandHandler("start", start))
+dispatcher.add_handler(CommandHandler("Price", Price))
 updater.start_polling()
